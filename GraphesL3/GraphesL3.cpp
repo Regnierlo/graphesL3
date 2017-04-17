194f6dd93a176ab0b1380f502becef3edb7136f8
Lois <lois.regnier.93@gmail.com>
2017-04-17 17:34:34 +0200

    Thread OK. Ajouter les traitements

--- GraphesL3/GraphesL3.cpp
+++ GraphesL3/GraphesL3.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <Windows.h>
 #include "Outils.h"
 
 using namespace std;
@@ -6,7 +9,32 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	//Création des processus
+		//Thread 1 -> traitement du fichier
+		thread t1([]() {
+			lireFichier("..\\FichiersCol\\queen11_11.col"); // appel fonction dans outils.cpp
+		});
 
-	lireFichier("..\\FichiersCol\\queen11_11.col"); // appel fonction dans outils.cpp
+		//Thread 2 -> action utilisateur
+		thread t2([](){
+			bool fin = false;
+			int nb = 100000000;
+
+
+			while (!fin)
+			{
+				if (GetKeyState(VK_END))
+				{
+					for (int i = 0; i < nb; i++)
+					{
+						cout << i << endl;
+					}
+					fin = true;
+				}
+			}
+		});
+
+		t1.join();
+		t2.join();
+
 	return 0;
 }
